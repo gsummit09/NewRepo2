1c6dc9ffcc87cf0cdc7d5ee7b9f52e30760edd6f
gsummit09 <nayaksumit998@gmail.com>
2024-02-01 18:00:54 +0530

    Add project files.

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment_feb01_MVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            ViewBag.msg = "Customer List";
+            List<String> list = new List<String>()
+            {
+                "Itishree",
+                "Sumit",
+                "priyanka",
+                "Biswa"
+            };
+            ViewBag.nameList=list;
+            return View();
+        }
+        [HttpGet]
+        public ActionResult RedirectDemo()
+        {
+            TempData["WelcomeMessage"] = "WelCome to our Application";
+            return RedirectToAction("Index");
+        }
+        
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Assignment_feb01_MVC.MvcApplication" Language="C#" %>
